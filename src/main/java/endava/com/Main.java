36673b3898863ae283621b3fadb05e0e8f31edd0
WarriorsSami <sbarbutdica@gmail.com>
2023-05-04 18:11:10 +0300

    build: init proj

---
+++ src/main/java/endava/com/Main.java
@@ -0,0 +1,31 @@
+package endava.com;
+
+//        1. Open the browser
+//
+//        2. Enter the URL http://practice.automationtesting.in/
+//
+//        3. Verify that the home page has three arrivals only in the 'new arrivals' section
+//
+//        4. Click on Shop Menu
+//
+//        5. Change 'Default sorting' to 'Sort by average rating'
+//
+//        6. Click on 'Mastering JavaScript'
+//
+//        7. Click on the ADD TO BASKET button
+//
+//        8. Check whether the VIEW BASKET button is visible and click on it
+//
+//        9. Now user can find total and subtotal values just above the Proceed to Checkout button, verify that the total is greater than the subtotal
+//
+//        10. Click the PROCEED TO CHECKOUT button
+//
+//        11. Fill in all required fields, set Cash on Delivery as your payment method, and click PLACE ORDER
+//
+//        12. Check if the Payment method is set to Cash on Delivery in both 'PAYMENT METHOD' fields in the 'Order Details' page
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
